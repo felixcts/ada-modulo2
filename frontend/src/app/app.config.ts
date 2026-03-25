d443f9d7fe9652a6cfb97b2c68caf5f66e7d07dd
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2026-03-24 21:16:48 -0300

    aula 06 finalizada

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { importProvidersFrom } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
@@ -6,6 +7,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +20,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideEnvironmentNgxMask(),
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    importProvidersFrom(
+      TranslateModule.forRoot()
+    ),
+    ...provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
   ],
 };
